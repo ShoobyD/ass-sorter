7344dec64bef501967e71e54f057ab3dd0b1f053
Baruch Mustakis <baruch@pic-time.com>
2019-01-07 15:45:48 +0200

    render

--- sort-my-ass.js
+++ sort-my-ass.js
@@ -11,19 +11,6 @@ function AssSorter( ELM_SEL ) {
 		return;
 	}
 
-	// Inheritance function
-	function inherits( ctor, superCtor ) {
-		ctor.super_    = superCtor;
-		ctor.prototype = Object.create( superCtor.prototype, {
-			'constructor' : {
-				'value'        : ctor,
-				'enumerable'   : false,
-				'writable'     : true,
-				'configurable' : true,
-			}
-		} );
-	};
-
 
 
 	/*==*==*==*==*==*==*==*==*
@@ -49,18 +36,24 @@ function AssSorter( ELM_SEL ) {
 		this.Text    = match[ 10 ];
 
 	};
+	Line.prototype.render = function() {
 
+		return $( `
+				<tr title="${ this.Start }">
+					<td class="name">${ this.Name }</td>
+					<td class="text">${ this.parseText() }&rlm;</td>
+				</tr>
+			` )
+			.addClass( 'line' );
 
+	};
+	Line.prototype.parseText = function() {
 
-	/*==*==*==*==*==*==*==*==*
-	 *      General fn       *
-	 *==*==*==*==*==*==*==*==*/
+		return this.Text
+			.split( '\\N' )
+			.join( '<br>' );
 
-	function isDigit( x ) {
-
-		return /^[0-9]$/.test( x );
-
-	}
+	};
 
 
 
@@ -86,18 +79,54 @@ function AssSorter( ELM_SEL ) {
 						] );
 						return;
 					}
+					if ( !/\.ass$/i.test( fileList[ 0 ].name ) ) {
+						dialog.open( 'Only ASS files are supported.' );
+						return;
+					}
+					handleFile( fileList[ 0 ] );
 				},
 			} );
 
 	}
 
+	function handleFile( file ) {
+		const reader = new FileReader();
+		reader.readAsText( file )
+		reader.onload = function() {
+			$uploader.hide();
+			parseResult( reader.result );
+		}
+	}
 
+	function parseResult( txt ) {
 
-	/*==*==*==*==*==*==*==*==*
-	 *       Handlers        *
-	 *==*==*==*==*==*==*==*==*/
+		testIsHebrew( txt );
+		let lines = txt.split( '\n' ).filter( l => /^Dialogue:/i.test( l ) );
+		lines = lines.map( l => new Line( l ) );
+		lines.sort( ( a, b ) => new Date( '1970/01/01 ' + a.Start ) - new Date('1970/01/01 ' + b.Start ) );
 
-	function BuildSudoku() {
+		$output
+			.html( renderLines( lines ) )
+			.show();
+	}
+
+	function testIsHebrew( txt ) {
+
+		if ( /[א-ת]/i.test( txt ) )
+			$output.addClass( 'hebrew-ass' );
+
+	}
+
+	function renderLines( lines ) {
+
+		return $( '<table>' ).html( lines.map( l => l.render() ) );
+
+	}
+
+	function reset() {
+
+		$output.empty().hide();
+		$uploader.show();
 
 	}
 
@@ -111,14 +140,19 @@ function AssSorter( ELM_SEL ) {
 	const dialog = window.Dialog? new Dialog(): { 'open' : alert.bind( window ) };
 
 	// DOM shit
-	const $wrap = $( ELM_SEL );
-	const uploader = setDroploader();
-
+	const $wrap     = $( ELM_SEL );
+	const $uploader = setDroploader();
+	const $output   = $( '<div id="output">' ).appendTo( $wrap );
+	$( '<button>' )
+		.html( 'Reset' )
+		.addClass( 'reset' )
+		.click( reset )
+		.prependTo( $wrap );
 
 
 	/** API **/
 	return {
-		//insert,
+		reset,
 	};
 
 }
